1981a8597e8b357cd77469e712e948429a2b7580
Hasan Moosavi <h.mousavi910@gmail.com>
2026-02-28 19:54:07 +0330

    Refactor Front

--- apps/front/src/lib/rtk/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@lib/rtk/store";
-import { TypedUseSelectorHook } from "react-redux";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
